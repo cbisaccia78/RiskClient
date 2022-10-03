9622a4872385a49247483e1291af9bdd45bf33fd
cbisaccia78 <cbisaccia78@gmail.com>
2022-10-03 16:27:06 -0400

    register form

--- src/components/UI/RegisterForm.js
+++ src/components/UI/RegisterForm.js
@@ -8,8 +8,10 @@ export default function(props){
     const authctx = useContext(AuthContext)
 
     const [submitClicked, setSubmitClicked] = useState(false)
+    const name = useRef('')
     const username = useRef('')
     const password = useRef('')
+    const email = useRef('')
     const pictureInputRef = useRef()
 
     const handleClose = function(){
@@ -23,15 +25,21 @@ export default function(props){
                 </Modal.Header>
 
                 <Modal.Body>
+                <label htmlFor="registerName">Full Name</label>
+                <input type="text" id="registerName" ref={name} />
+
                 <label htmlFor="registerUsername">Username</label>
-                <input type="text" id="registerUsername" ref={username}>
-                </input>
+                <input type="text" id="registerUsername" ref={username} />
+
                 <label htmlFor="registerPassword">Password</label>
-                <input type="password" id="registerPassword" ref={password}>
-                </input>
+                <input type="password" id="registerPassword" ref={password} />
+
+                <label htmlFor="registerEmail">Email</label>
+                <input type="text" id="registerEmail" ref={email} />
+
                 <label htmlFor="registerImage">Profile Picture</label>
-                <input type="file" id="registerImage" accept="image/png, image/jpeg">
-                </input>
+                <input type="file" id="registerImage" accept="image/png, image/jpeg" />
+
                 </Modal.Body>
 
                 <Modal.Footer>
